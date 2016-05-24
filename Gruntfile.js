6e4d7b4404258fac20c7b976d10ace6897ea6827
Marco Solazzi <marco.solazzigmail.com>
2016-05-24 11:00:15 +0200

    updated deps

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,121 +2,123 @@
  * grunt-nodemailer
  * https://github.com/dwightjack/grunt-nodemailer
  *
- * Copyright (c) 2013 Marco Solazzi
+ * Copyright (c) 2013-2016 Marco Solazzi
  * Licensed under the MIT license.
  */
 
 'use strict';
 
-module.exports = function(grunt) {
+/* eslint-disable func-names, camelcase */
+module.exports = function (grunt) {
 
-  var filestubTransport = require('./tasks/transports/fake');
+    var filestubTransport = require('./tasks/transports/fake');
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js'
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
+    // Project configuration.
+    grunt.initConfig({
+        eslint: {
+            all: {
+                src: [
+                    'Gruntfile.js',
+                    'tasks/*.js',
+                    '<%= nodeunit.tests %>'
+                ]
+            }
+        },
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+            tests: ['tmp']
+        },
 
-    //Configuration to be run (and then tested).
-    nodemailer: {
+        //Configuration to be run (and then tested).
+        nodemailer: {
 
-      options: {
-        transport: filestubTransport,
-        recipients: ['john.doe@gmail.com']
-      },
+            options: {
+                transport: filestubTransport,
+                recipients: ['john.doe@gmail.com']
+            },
 
-      simple_message: {
-        options: {
-          message: {
-            subject: 'simple_message',
-            html: '<h1>Test Message</h1>',
-            text: 'test fallback'
-          }
+            simple_message: {
+                options: {
+                    message: {
+                        subject: 'simple_message',
+                        html: '<h1>Test Message</h1>',
+                        text: 'test fallback'
+                    }
+                }
+            },
+
+            mixed_recipients: {
+                options: {
+                    message: {
+                        subject: 'mixed_recipients',
+                        html: '<h1>Test Message</h1>',
+                        text: 'test fallback',
+                        to: 'myself@gmail.com'
+                    },
+                    recipients: [{
+                        name: 'John Doe',
+                        email: 'john.doe@gmail.com'
+                    }]
+                }
+            },
+
+            external_sources: {
+                options: {
+                    subject: 'external_sources',
+                    message: {
+                        subject: 'external_sources',
+                        html: '<h1>Test Message</h1>',
+                        text: 'test fallback'
+                    }
+                },
+                src: ['test/fixtures/*.html']
+            },
+
+            external_sources_txt: {
+                options: {
+                    subject: 'external_sources_txt',
+                    message: {
+                        subject: 'external_sources_txt',
+                        text: 'test fallback txt'
+                    }
+                },
+                src: ['test/fixtures/email-body-as-txt.txt']
+            },
+
+            external_sources_md: {
+                options: {
+                    subject: 'external_sources_md',
+                    message: {
+                        subject: 'external_sources_md',
+                        text: 'test fallback md'
+                    }
+                },
+                src: ['test/fixtures/*.md']
+            }
+        },
+
+        // Unit tests.
+        nodeunit: {
+            tests: ['test/*_test.js']
         }
-      },
 
-      mixed_recipients: {
-        options: {
-          message: {
-            subject: 'mixed_recipients',
-            html: '<h1>Test Message</h1>',
-            text: 'test fallback',
-            to: 'myself@gmail.com'
-          },
-          recipients: [{
-            name: 'John Doe',
-            email: 'john.doe@gmail.com'
-          }]
-        }
-      },
+    });
 
-      external_sources: {
-        options: {
-          subject: 'external_sources',
-          message: {
-            subject: 'external_sources',
-            html: '<h1>Test Message</h1>',
-            text: 'test fallback'
-          }
-        },
-        src: ['test/fixtures/*.html']
-      },
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
 
-      external_sources_txt: {
-        options: {
-          subject: 'external_sources_txt',
-          message: {
-            subject: 'external_sources_txt',
-            text: 'test fallback txt'
-          }
-        },
-        src: ['test/fixtures/email-body-as-txt.txt']
-      },
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-eslint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-      external_sources_md: {
-        options: {
-          subject: 'external_sources_md',
-          message: {
-            subject: 'external_sources_md',
-            text: 'test fallback md'
-          }
-        },
-        src: ['test/fixtures/*.md']
-      }
-    },
+    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+    // plugin's task(s), then test the result.
+    grunt.registerTask('test', ['clean', 'nodemailer', 'nodeunit']);
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    }
-
-  });
-
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
-
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'nodemailer', 'nodeunit']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['eslint', 'test']);
 
 };
+/* eslint-enable func-names, camelcase */
--- tasks/nodemailer.js
+++ tasks/nodemailer.js
@@ -2,155 +2,155 @@
  * grunt-nodemailer
  * https://github.com/dwightjack/grunt-nodemailer
  *
- * Copyright (c) 2013 Marco Solazzi
+ * Copyright (c) 2013-2016 Marco Solazzi
  * Licensed under the MIT license.
  */
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
 
-  var path = require('path'),
-      util = require('util'),
-      _ = require('lodash'),
-      async = require('async'),
-      nodemailer = require('nodemailer'),
-      sendmailTransport;
+    var path = require('path'),
+        util = require('util'),
+        _ = require('lodash'),
+        async = require('async'),
+        nodemailer = require('nodemailer'),
+        sendmailTransport;
 
 
-  try {
-    sendmailTransport = require('nodemailer-sendmail-transport');
-  } catch (e) {
-    sendmailTransport = require('nodemailer/node_modules/nodemailer-sendmail-transport');
-  }
-
-  grunt.registerMultiTask('nodemailer', 'Grunt wrapper for Nodemailer', function() {
-    var done = this.async(),
-      messages = [],
-      options = this.options({
-        transport: null,
-        message: {},
-        recipients: [],
-        from: '', //DEPRECATED
-        subject: '', //DEPRECATED
-        html: '', //DEPRECATED
-        text: '' //DEPRECATED
-      }),
-      defaultMessage,
-      transport,
-      subject;
-
-    defaultMessage = _.defaults(options.message, {
-        to: [],
-        from: 'nodemailer <sender@example.com>',
-        subject: '',
-        html: '',
-        text: ''
-    });
-    // Override files provided in gruntfile if filepath added in grunt command
-    var files =  grunt.option('fileSrc') ? [grunt.option('fileSrc')] : this.filesSrc;
-
-    //ensure recipients are an array
-    if (!Array.isArray(defaultMessage.to)) {
-      defaultMessage.to = defaultMessage.to.split(',');
+    try {
+        sendmailTransport = require('nodemailer-sendmail-transport');
+    } catch (e) {
+        sendmailTransport = require('nodemailer/node_modules/nodemailer-sendmail-transport');
     }
 
-    if (options.transport) {
-      transport = nodemailer.createTransport(options.transport);
-    } else {
-      transport = nodemailer.createTransport(sendmailTransport);
-    }
+    grunt.registerMultiTask('nodemailer', 'Grunt wrapper for Nodemailer', function taskRegistration() {
+        var done = this.async(),
+            messages = [],
+            options = this.options({
+                transport: null,
+                message: {},
+                recipients: [],
+                from: '', //DEPRECATED
+                subject: '', //DEPRECATED
+                html: '', //DEPRECATED
+                text: '' //DEPRECATED
+            }),
+            defaultMessage,
+            transport,
+            files;
 
-    _.each(options.recipients, function(el) {
-      var args = ['"%s" <%s>'];
-      if (typeof el === 'string') {
-        args.push(el, el);
-      } else {
-        args.push(el.name, el.email);
-      }
-      defaultMessage.to.push( util.format.apply(null, args) );
-    });
+        defaultMessage = _.defaults(options.message, {
+            to: [],
+            from: 'nodemailer <sender@example.com>',
+            subject: '',
+            html: '',
+            text: ''
+        });
+        // Override files provided in gruntfile if filepath added in grunt command
+        files = grunt.option('fileSrc') ? [grunt.option('fileSrc')] : this.filesSrc;
 
-    if (defaultMessage.to.length < 1) {
-      grunt.fail.fatal('No recipient provided.');
-    }
-
-    //process deprecated options
-    //TODO: remove in next version
-    _.each(['from','subject','html','text'], function (prop) {
-      if (!_.isEmpty(options[prop])) {
-        grunt.verbose.writeln(('Option "' + prop + '" is DEPRECATED. Please, set "message.' + prop + '" instead.').yellow);
-        defaultMessage[prop] = options[prop];
-      }
-    });
-
-
-    if (files.length) {
-      //When a file array is provided, use sources
-      //as HTML/TXT message input
-      //Multiple sources files will generate multiple messages
-      messages = _.map(files, function (f) {
-        var ext,
-            txtFile,
-            newMsg = false;
-
-        if (grunt.file.isFile(f)) {
-
-          ext = path.extname(f);
-
-          if (/^\.html?$/i.test(ext)) {
-
-            newMsg = _.clone(defaultMessage);
-            newMsg.html = grunt.file.read(f);
-
-            //override subject with HTML title tag
-            if (/<title>([^<]+)<\/title>/i.test(newMsg.html)) {
-              newMsg.subject = newMsg.html.match(/<title>([^<]+)<\/title>/i)[1];
-            }
-
-            //when dealing with .html files, lookup for txt files with same filename
-            txtFile = path.join(path.dirname(f), path.basename(f, ext) + '.txt');
-            if (grunt.file.isFile(txtFile)) {
-              newMsg.text = grunt.file.read(txtFile);
-            }
-
-          } else {
-            newMsg = _.clone(defaultMessage);
-            newMsg.text = grunt.file.read(f);
-          }
-
+        //ensure recipients are an array
+        if (!Array.isArray(defaultMessage.to)) {
+            defaultMessage.to = defaultMessage.to.split(',');
         }
 
-        return newMsg;
-
-      });
-
-    } else {
-      messages.push(defaultMessage);
-    }
-
-
-    grunt.log.writeln(util.format('Sending %d e-mail%s to recipients: %s', messages.length, (messages.length > 1 ? 's': ''), defaultMessage.to.join(', ') ));
-
-    //Sending messages in series (which is slower) since some
-    //services may limit concurrent connections.
-    async.eachSeries(messages, function (msg, next) {
-      transport.sendMail(msg, function(error, responseStatus) {
-        if (error) {
-          grunt.verbose.errorlns(JSON.stringify(error));
-          grunt.fail.fatal('Error occured: ' + error.message);
-          next(error);
-          return;
+        if (options.transport) {
+            transport = nodemailer.createTransport(options.transport);
+        } else {
+            transport = nodemailer.createTransport(sendmailTransport);
         }
-        grunt.log.writeln(('Message sent successfully.').green);
-        next(null);
-      });
-    }, done);
+
+        _.each(options.recipients, function recipientsIterator(el) {
+            var args = ['"%s" <%s>'];
+            if (typeof el === 'string') {
+                args.push(el, el);
+            } else {
+                args.push(el.name, el.email);
+            }
+            defaultMessage.to.push(util.format.apply(null, args));
+        });
+
+        if (defaultMessage.to.length < 1) {
+            grunt.fail.fatal('No recipient provided.');
+        }
+
+        //process deprecated options
+        //TODO: remove in next version
+        _.each(['from', 'subject', 'html', 'text'], function propIterator(prop) {
+            if (!_.isEmpty(options[prop])) {
+                grunt.verbose.writeln(('Option "' + prop + '" is DEPRECATED. Please, set "message.' + prop + '" instead.').yellow);
+                defaultMessage[prop] = options[prop];
+            }
+        });
 
 
-  });
+        if (files.length) {
+            //When a file array is provided, use sources
+            //as HTML/TXT message input
+            //Multiple sources files will generate multiple messages
+            messages = _.map(files, function filesIterator(f) {
+                var ext,
+                    txtFile,
+                    newMsg = false;
+
+                if (grunt.file.isFile(f)) {
+
+                    ext = path.extname(f);
+
+                    if (/^\.html?$/i.test(ext)) {
+
+                        newMsg = _.clone(defaultMessage);
+                        newMsg.html = grunt.file.read(f);
+
+                        //override subject with HTML title tag
+                        if (/<title>([^<]+)<\/title>/i.test(newMsg.html)) {
+                            newMsg.subject = newMsg.html.match(/<title>([^<]+)<\/title>/i)[1];
+                        }
+
+                        //when dealing with .html files, lookup for txt files with same filename
+                        txtFile = path.join(path.dirname(f), path.basename(f, ext) + '.txt');
+                        if (grunt.file.isFile(txtFile)) {
+                            newMsg.text = grunt.file.read(txtFile);
+                        }
+
+                    } else {
+                        newMsg = _.clone(defaultMessage);
+                        newMsg.text = grunt.file.read(f);
+                    }
+
+                }
+
+                return newMsg;
+
+            });
+
+        } else {
+            messages.push(defaultMessage);
+        }
+
+
+        grunt.log.writeln(util.format('Sending %d e-mail%s to recipients: %s', messages.length, (messages.length > 1 ? 's' : ''), defaultMessage.to.join(', ')));
+
+        //Sending messages in series (which is slower) since some
+        //services may limit concurrent connections.
+        async.eachSeries(messages, function asyncFn(msg, next) {
+            transport.sendMail(msg, function sendCb(error/*, responseStatus*/) {
+                if (error) {
+                    grunt.verbose.errorlns(JSON.stringify(error));
+                    grunt.fail.fatal('Error occured: ' + error.message);
+                    next(error);
+                    return;
+                }
+                grunt.log.writeln(('Message sent successfully.').green);
+                next(null);
+            });
+        }, done);
+
+
+    });
 
 };
